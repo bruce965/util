8589a89aed1bc281eb79baedc98146fde69dfe3c
Fabio Iotti <fabiogiopla@gmail.com>
2022-05-27 17:07:00 +0200

    Improve Boxed

--- CSharp/Language/Boxed.cs
+++ CSharp/Language/Boxed.cs
@@ -2,29 +2,26 @@
 // The copyright holders license this file to you under the MIT license,
 // available at https://github.com/bruce965/util/raw/master/LICENSE
 
-using UnityEngine;
+namespace Utility;
 
-namespace Utility
+/// <summary>
+/// Boxed reference to a <typeparamref name="T"/>, resides on the heap.
+/// </summary>
+/// <typeparam name="T">Type of the boxed value.</typeparam>
+public class Boxed<T>
 {
-    /// <summary>
-    /// Boxed reference to a value, resides on the heap.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class Boxed<T>
-    {
-        T _value;
-        public ref T Value => ref _value;
+    T _value;
+    public ref T Value => ref _value;
 
-        public Boxed()
-            => Value = default;
+    //public Boxed()
+    //    => _value = default;
 
-        public Boxed(T value)
-            => Value = value;
+    public Boxed(T value)
+        => _value = value;
 
-        public static implicit operator T(Boxed<T> box)
-            => box.Value;
+    public static implicit operator T(Boxed<T> box)
+        => box.Value;
 
-        public static explicit operator Boxed<T>(T value)
-            => new Boxed<T>(value);
-    }
+    public static implicit operator Boxed<T>(T value)
+        => new(value);
 }
