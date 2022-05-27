d1c55842c678836892f5816ed5ded08c2444091b
Fabio Iotti <fabiogiopla@gmail.com>
2022-05-27 17:07:14 +0200

    Fix EnumValues

--- CSharp/Language/EnumValues.cs
+++ CSharp/Language/EnumValues.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2021 Fabio Iotti
+﻿// Copyright (c) 2021-2022 Fabio Iotti
 // The copyright holders license this file to you under the MIT license,
 // available at https://github.com/bruce965/util/raw/master/LICENSE
 
@@ -7,39 +7,38 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Utility
+namespace Utility;
+
+public static class EnumValues
 {
-    public static class EnumValues
+    class EnumValuesOf<T> : IReadOnlyList<T>
+        where T : Enum
     {
-        class EnumValues<T> : IReadOnlyList<T>
-            where T : Enum
-        {
-            static readonly T[] values
-                = Enum.GetValues(typeof(T)).Cast<T>().ToArray();
+        static readonly T[] values
+            = Enum.GetValues(typeof(T)).Cast<T>().ToArray();
 
-            public static EnumValues<T> Instance
-                = new EnumValues<T>();
+        public static EnumValuesOf<T> Instance
+            = new();
 
-            public int Count => values.Length;
+        public int Count => values.Length;
 
-            public T this[int index]
-                => values[index];
+        public T this[int index]
+            => values[index];
 
-            EnumValues() { }
+        EnumValuesOf() { }
 
-            public IEnumerator<T> GetEnumerator()
-                => ((IEnumerable<T>)values).GetEnumerator();
+        public IEnumerator<T> GetEnumerator()
+            => ((IEnumerable<T>)values).GetEnumerator();
 
-            IEnumerator IEnumerable.GetEnumerator()
-                => values.GetEnumerator();
-        }
-
-        /// <summary>
-        /// Read-only equivalent of <see cref="Enum.GetValues(Type)"/>, without temporary allocations.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        public static IReadOnlyList<T> ValuesOf<T>() where T : Enum
-            => EnumValues<T>.Instance;
+        IEnumerator IEnumerable.GetEnumerator()
+            => values.GetEnumerator();
     }
+
+    /// <summary>
+    /// Read-only equivalent of <see cref="Enum.GetValues(Type)"/>, without temporary allocations.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static IReadOnlyList<T> ValuesOf<T>() where T : Enum
+        => EnumValuesOf<T>.Instance;
 }
