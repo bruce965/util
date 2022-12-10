8718b82708c2a7bd121016b2f480ff5bded5e551
Fabio Iotti <fabiogiopla@gmail.com>
2022-12-10 17:14:37 +0100

    Improve C# Assert

--- CSharp/Diagnostics/Assert.cs
+++ CSharp/Diagnostics/Assert.cs
@@ -2,6 +2,8 @@
 // The copyright holders license this file to you under the MIT license,
 // available at https://github.com/bruce965/util/raw/master/LICENSE
 
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
@@ -9,6 +11,12 @@ using System.Text.RegularExpressions;
 
 namespace Utility;
 
+/// <summary>
+/// Utilities to make assertions evaluated during execution.
+/// <para>
+/// Unlike <see cref="Debug.Assert"/>, these assertions are not ignored in release builds.
+/// </para>
+/// </summary>
 public static class Assert
 {
     static readonly Regex MayBeCompositeExpression = new(@"[-+*/&|^~%?:<>=!]", RegexOptions.Compiled);
@@ -18,14 +26,14 @@ public static class Assert
     /// </summary>
     /// <param name="argument">The reference type argument to validate as non-null.</param>
     /// <param name="message">The error message that explains the reason for the exception.</param>
-    /// <param name="_paramName">The name of the parameter with which <paramref name="argument"/> corresponds (generated automatically from the compiler).</param>
+    /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds (added automatically by the compiler).</param>
     /// <exception cref="ArgumentNullException">Thrown if <paramref name="argument"/> is <c>null</c>.</exception>
-    public static void ArgumentNotNull([NotNull] object? argument, string? message = null, [CallerArgumentExpression("argument")] string _paramName = null!)
+    public static T ArgumentNotNull<T>([NotNull] T? argument, string? message = null, [CallerArgumentExpression("argument")] string? paramName = null)
     {
         if (argument is not null)
-            return;
+            return argument;
 
-        throw new ArgumentNullException(_paramName, message);
+        throw new ArgumentNullException(paramName, message);
     }
 
     /// <summary>
@@ -34,15 +42,37 @@ public static class Assert
     /// <param name="argument">The reference type argument to check the condition on.</param>
     /// <param name="condition">The condition to be checked.</param>
     /// <param name="message">The error message that explains the reason for the exception.</param>
-    /// <param name="_paramName">The name of the parameter with which <paramref name="argument"/> corresponds (generated automatically from the compiler).</param>
+    /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds (added automatically by the compiler).</param>
     /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="condition"/> is <c>false</c>.</exception>
-    [SuppressMessage("Style", "IDE0060")]
-    public static void ArgumentInRange(object? argument, [DoesNotReturnIf(false)] bool condition, string? message = null, [CallerArgumentExpression("argument")] string _paramName = null!)
+    public static T ArgumentInRange<T>(T argument, [DoesNotReturnIf(false)] bool condition, string? message = null, [CallerArgumentExpression("argument")] string? paramName = null)
+    {
+        ArgumentInRange(condition, message, paramName);
+        return argument;
+    }
+
+    /// <inheritdoc cref="ArgumentInRange{T}(T, bool, string?, string)"/>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static Span<T> ArgumentInRange<T>(Span<T> argument, [DoesNotReturnIf(false)] bool condition, string? message = null!, [CallerArgumentExpression("argument")] string? paramName = null)
+    {
+        ArgumentInRange(condition, message, paramName);
+        return argument;
+    }
+
+    /// <inheritdoc cref="ArgumentInRange{T}(T, bool, string?, string)"/>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static ReadOnlySpan<T> ArgumentInRange<T>(ReadOnlySpan<T> argument, [DoesNotReturnIf(false)] bool condition, string? message = null, [CallerArgumentExpression("argument")] string? paramName = null)
+    {
+        ArgumentInRange(condition, message, paramName);
+        return argument;
+    }
+
+    [StackTraceHidden]
+    static void ArgumentInRange([DoesNotReturnIf(false)] bool condition, string? message, string? paramName)
     {
         if (condition)
             return;
 
-        throw new ArgumentOutOfRangeException(_paramName, message);
+        throw new ArgumentOutOfRangeException(paramName, message);
     }
 
     /// <summary>
@@ -51,15 +81,37 @@ public static class Assert
     /// <param name="param">Reference to the parameter.</param>
     /// <param name="condition">Condition to be checked.</param>
     /// <param name="message">The error message that explains the reason for the exception.</param>
-    /// <param name="_paramName">The name of the parameter with which <paramref name="argument"/> corresponds (generated automatically from the compiler).</param>
+    /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds (added automatically by the compiler).</param>
     /// <exception cref="ArgumentException">Thrown if <paramref name="condition"/> is <c>false</c>.</exception>
-    [SuppressMessage("Style", "IDE0060")]
-    public static void Argument(object? param, [DoesNotReturnIf(false)] bool condition, string? message = null, [CallerArgumentExpression("param")] string _paramName = null!)
+    public static T Argument<T>(T param, [DoesNotReturnIf(false)] bool condition, string? message = null, [CallerArgumentExpression("param")] string? paramName = null)
+    {
+        Argument(condition, message, paramName);
+        return param;
+    }
+
+    /// <inheritdoc cref="Argument{T}(T, bool, string?, string)"/>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static Span<T> Argument<T>(Span<T> param, [DoesNotReturnIf(false)] bool condition, string? message = null, [CallerArgumentExpression("param")] string? paramName = null)
+    {
+        Argument(condition, message, paramName);
+        return param;
+    }
+
+    /// <inheritdoc cref="Argument{T}(T, bool, string?, string)"/>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static ReadOnlySpan<T> Argument<T>(ReadOnlySpan<T> param, [DoesNotReturnIf(false)] bool condition, string? message = null, [CallerArgumentExpression("param")] string? paramName = null)
+    {
+        Argument(condition, message, paramName);
+        return param;
+    }
+
+    [StackTraceHidden]
+    static void Argument([DoesNotReturnIf(false)] bool condition, string? message, string? paramName)
     {
         if (condition)
             return;
 
-        throw new ArgumentException(message, _paramName);
+        throw new ArgumentException(message, paramName);
     }
 
     /// <summary>
@@ -67,14 +119,15 @@ public static class Assert
     /// </summary>
     /// <param name="condition">Condition to be checked.</param>
     /// <param name="message">The error message that explains the reason for the exception.</param>
-    /// <param name="_expression">The condition expression (generated automatically from the compiler).</param>
+    /// <param name="expression">The condition expression (added automatically by the compiler).</param>
     /// <exception cref="AssertionFailedException">Thrown if <paramref name="condition"/> is <c>false</c>.</exception>
-    public static void That([DoesNotReturnIf(false)] bool condition, string? message = null, [CallerArgumentExpression("condition")] string _expression = null!)
+    public static void That([DoesNotReturnIf(false)] bool condition, string? message = null, [CallerArgumentExpression("condition")] string? expression = null)
     {
         if (condition)
             return;
 
-        Fail(message, _expression);
+        Fail(message, expression);
+        throw null; // will never happen
     }
 
     /// <summary>
@@ -82,14 +135,15 @@ public static class Assert
     /// </summary>
     /// <param name="obj">Null object.</param>
     /// <param name="message">The error message that explains the reason for the exception.</param>
-    /// <param name="_expression">The null expression (generated automatically from the compiler).</param>
+    /// <param name="expression">The null expression (added automatically by the compiler).</param>
     /// <exception cref="AssertionFailedException">Thrown if <paramref name="obj"/> is not <c>null</c>.</exception>
-    public static void Null(object? obj, string? message = null, [CallerArgumentExpression("obj")] string _expression = null!)
+    public static void Null<T>(T obj, string? message = null, [CallerArgumentExpression("obj")] string? expression = null)
     {
         if (obj is null)
             return;
 
-        Fail(message, Wrap("{0} is null", _expression));
+        Fail(message, WrapExpression("{0} is null", expression));
+        throw null; // will never happen
     }
 
     /// <summary>
@@ -97,14 +151,15 @@ public static class Assert
     /// </summary>
     /// <param name="obj">Non-null object.</param>
     /// <param name="message">The error message that explains the reason for the exception.</param>
-    /// <param name="_expression">The non-null expression (generated automatically from the compiler).</param>
+    /// <param name="expression">The non-null expression (added automatically by the compiler).</param>
     /// <exception cref="AssertionFailedException">Thrown if <paramref name="obj"/> is <c>null</c>.</exception>
-    public static void NotNull([NotNull] object? obj, string? message = null, [CallerArgumentExpression("obj")] string _expression = null!)
+    public static T NotNull<T>([NotNull] T obj, string? message = null, [CallerArgumentExpression("obj")] string? expression = null)
     {
         if (obj is not null)
-            return;
+            return obj;
 
-        Fail(message, Wrap("{0} is not null", _expression));
+        Fail(message, WrapExpression("{0} is not null", expression));
+        throw null; // will never happen
     }
 
     /// <summary>
@@ -113,33 +168,78 @@ public static class Assert
     /// <typeparam name="T">Type to be casted to.</typeparam>
     /// <param name="obj">Object to be casted to <typeparamref name="T"/>.</param>
     /// <param name="message">The error message that explains the reason for the exception.</param>
-    /// <param name="_expression">The expression the value of which to be casted to <typeparamref name="T"/> (generated automatically from the compiler).</param>
+    /// <param name="expression">The expression the value of which to be casted to <typeparamref name="T"/> (added automatically by the compiler).</param>
     /// <returns><paramref name="obj"/> casted to <typeparamref name="T"/>.</returns>
     /// <exception cref="AssertionFailedException">Thrown if <paramref name="obj"/> couldn't be casted to <typeparamref name="T"/>.</exception>
-    public static T Cast<T>(object? obj, string? message = null, [CallerArgumentExpression("obj")] string _expression = null!)
+    public static T Cast<T>(object? obj, string? message = null, [CallerArgumentExpression("obj")] string? expression = null)
     {
         if (obj is T casted)
             return casted;
 
-        Fail(message, Wrap("{0} is {1}", _expression, typeof(T).FullName));
-        return default;  // will never happen
+        Fail(message, WrapExpression("{0} is {1}", expression, typeof(T).FullName));
+        throw null; // will never happen
+    }
+
+    /// <summary>
+    /// Throws an <see cref="AssertionFailedException"/> if the specified <see cref="object"/> instances are not considered equal.
+    /// </summary>
+    /// <param name="objA">The first object to compare.</param>
+    /// <param name="objB">The second object to compare.</param>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="expressionA">The expression of the first argument (added automatically by the compiler).</param>
+    /// <param name="expressionB">The expression of the second argument (added automatically by the compiler).</param>
+    /// <returns><paramref name="objB"/>.</returns>
+    /// <exception cref="AssertionFailedException">Thrown if <paramref name="objA"/> and <paramref name="objB"/> are not considered equal, not thrown if both are <see langword="null"/>.</exception>
+    [return: NotNullIfNotNull(nameof(objB))]
+    public static T Equal<T>([NotNullIfNotNull("objB")] T objA, [NotNullIfNotNull("objA")] T objB, string? message = null, [CallerArgumentExpression("objA")] string? expressionA = null, [CallerArgumentExpression("objB")] string? expressionB = null)
+    {
+        if (object.Equals(objA, objB))
+            return objB;
+
+        Fail(message, WrapExpression("Equals({0}, {1})", expressionA, expressionB));
+        throw null; // will never happen
+    }
+
+    /// <summary>
+    /// Throws an <see cref="AssertionFailedException"/> if the specified instances are not the same instance.
+    /// </summary>
+    /// <param name="objA">The first object to compare.</param>
+    /// <param name="objB">The second object to compare.</param>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="expressionA">The expression of the first argument (added automatically by the compiler).</param>
+    /// <param name="expressionB">The expression of the second argument (added automatically by the compiler).</param>
+    /// <returns><param name="objA"> and <param name="objB">.</returns>
+    /// <exception cref="AssertionFailedException">Thrown if <paramref name="objA"/> is not the same instance as <paramref name="objB"/>, not thrown if both are <see langword="null"/>.</exception>
+    [return: NotNullIfNotNull(nameof(objA)), NotNullIfNotNull(nameof(objB))]
+    public static T ReferenceEqual<T>([NotNullIfNotNull("objB")] T objA, [NotNullIfNotNull("objA")] T objB, string? message = null, [CallerArgumentExpression("objA")] string? expressionA = null, [CallerArgumentExpression("objB")] string? expressionB = null)
+    {
+        if (object.ReferenceEquals(objA, objB))
+            return objA;
+
+        Fail(message, WrapExpression("ReferenceEquals({0}, {1})", expressionA, expressionB));
+        throw null; // will never happen
     }
 
     /// <summary>
     /// Throws an <see cref="AssertionFailedException"/>.
     /// </summary>
     /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <returns>This method never returns; the return type enables usage of the <see langword="throw"/> keyword.</returns>
     /// <exception cref="AssertionFailedException">Always thrown.</exception>
     [DoesNotReturn]
-    public static void Fail(string? message = null)
-        => Fail(message, null);
+    public static Exception Fail(string? message = null)
+    {
+        Fail(message, null);
+        throw null; // will never happen
+    }
 
+    [StackTraceHidden]
     [DoesNotReturn]
     static void Fail(string? message, string? expression)
-        => throw new AssertionFailedException(expression, message);
+        => throw new AssertionFailedException(message, expression);
 
-    [return: NotNullIfNotNull("expression")]
-    static string? Wrap(string format, string? expression, params object?[] args)
+    [return: NotNullIfNotNull(nameof(expression))]
+    static string? WrapExpression(string format, string? expression, params object?[] args)
     {
         if (expression == null)
             return null;
@@ -152,23 +252,55 @@ public static class Assert
 
         return string.Format(format, new[] { expression }.Concat(args).ToArray());
     }
+
+    /// <inheritdoc cref="object.Equals"/>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static new bool Equals(object? objA, object? objB)
+        => object.Equals(objA, objB);
+
+    /// <inheritdoc cref="object.ReferenceEquals"/>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static new bool ReferenceEquals(object? objA, object? objB)
+        => object.ReferenceEquals(objA, objB);
 }
 
+/// <summary>
+/// Represents assertions that failed during execution.
+/// </summary>
 [Serializable]
 public class AssertionFailedException : Exception
 {
-    public virtual string? Expression { get; }
+    public AssertionFailedException() : this(null, null, null) { }
 
-    public AssertionFailedException() : this(null) { }
+    public AssertionFailedException(string? message) : this(message, null, null) { }
 
-    public AssertionFailedException(string? expression) : this(expression, message: null) { }
+    public AssertionFailedException(string? message, Exception? innerException) : this(message, null, innerException) { }
 
-    public AssertionFailedException(string? message, Exception? inner) : base(message ?? "Assertion failed.", inner) { }
+    public AssertionFailedException(string? message, string? expression) : this(message, expression, null) { }
 
-    public AssertionFailedException(string? expression, string? message) : base((message ?? "Assertion failed.") + (expression == null ? "" : $"{Environment.NewLine}Expression: {expression}"))
+    public AssertionFailedException(string? message, string? expression, Exception? innerException) : base(message ?? "Assertion failed.", innerException)
     {
         Expression = expression;
     }
 
-    protected AssertionFailedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    protected AssertionFailedException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+        Expression = info.GetString(nameof(Expression));
+    }
+
+    /// <inheritdoc/>
+    public override string Message
+        => Expression == null ? base.Message : $"{base.Message} (Expression '{Expression}')";
+
+    /// <summary>
+    /// Gets a string representation of the expression for which the assertion failed.
+    /// </summary>
+    /// <value>String representation of the expression for which the assertion failed, or <see langword="null"/> if not available.</value>
+    public virtual string? Expression { get; }
+
+    public override void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        base.GetObjectData(info, context);
+        info.AddValue(nameof(Expression), Expression, typeof(string));
+    }
 }
